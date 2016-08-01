ff17fef2bbfd272cff11d0bd5cc8fe06c2a12e42
Constantin Gerstberger <constantin.gerstberger@xaasify.com>
2016-08-01 18:05:46 +0200

    reduce example query to two tables and add examples using the other join variants

--- src/main/scala/quillplayground/Main.scala
+++ src/main/scala/quillplayground/Main.scala
@@ -31,21 +31,21 @@ object Main extends App {
   }
 
   // ===========================================================================================================================================================
-  // QUERY
+  // QUERY-0
   // ===========================================================================================================================================================
-  val topProductNames = quote {
+  val topProductNames0 = quote {
     (for {
-      (category, product) <- Categories.join(Products).on(_.id == _.categoryId)
-      // comment the third join -> the query compiles
-      comment <- Comments.join(_.productId == product.id)
-    } yield (product.rating, product.name)).sortBy(_._1)(Ord.desc).take(100)
+      category <- Categories
+      product <- Products if category.id == product.categoryId
+    } yield {
+      (product.name, product.rating)
+    })
+      .sortBy(_._2)(Ord.desc)
   }
 
-  mysqlDatabase run topProductNames
-
   /* compile error message:
-  Error:(44, 17) exception during macro expansion:
-  java.lang.IllegalStateException: The monad composition can't be expressed using applicative joins. Faulty expression: 'x2.rating'. Free variables: 'List(x2)'., Faulty expression: 'x3x3'. Free variables: 'List(x3x3)'.
+  Error:(_, 17) exception during macro expansion:
+  java.lang.IllegalStateException: The monad composition can't be expressed using applicative joins. Faulty expression: 'category.id == product.categoryId'. Free variables: 'List(category)'.
     at io.getquill.util.Messages$.fail(Messages.scala:8)
     at io.getquill.context.sql.Prepare$$anonfun$1.apply(Prepare.scala:29)
     at io.getquill.context.sql.Prepare$$anonfun$1.apply(Prepare.scala:29)
@@ -58,7 +58,63 @@ object Main extends App {
     at io.getquill.context.ContextMacro$class.runExpr(ContextMacro.scala:51)
     at io.getquill.context.ContextMacro$class.run(ContextMacro.scala:21)
     at io.getquill.context.sql.SqlContextMacro.run(SqlContextMacro.scala:13)
-    mysqlDatabase run topProductNames
+    mysqlDatabase run topProductNames0
   */
+
+  // ===========================================================================================================================================================
+  // QUERY-1
+  // ===========================================================================================================================================================
+  val topProductNames4 = quote {
+    Categories
+      .flatMap(category =>
+        Products
+          .withFilter(product => category.id == product.categoryId)
+          .map(product => (product.name, product.rating))
+      )
+      .sortBy(_._2)
+  }
+
+  /* compile error message:
+  Error:(_, 17) exception during macro expansion:
+  java.lang.IllegalStateException: The monad composition can't be expressed using applicative joins. Faulty expression: 'category.id == product.categoryId'. Free variables: 'List(category)'.
+    at io.getquill.util.Messages$.fail(Messages.scala:8)
+    at io.getquill.context.sql.Prepare$$anonfun$1.apply(Prepare.scala:29)
+    at io.getquill.context.sql.Prepare$$anonfun$1.apply(Prepare.scala:29)
+    at scala.Option.map(Option.scala:146)
+    at io.getquill.context.sql.Prepare$.apply(Prepare.scala:29)
+    at io.getquill.context.sql.SqlContextMacro.prepare(SqlContextMacro.scala:19)
+    at io.getquill.context.QueryMacro$class.runQuery(QueryMacro.scala:48)
+    at io.getquill.context.sql.SqlContextMacro.runQuery(SqlContextMacro.scala:13)
+    at io.getquill.context.ContextMacro$class.run(ContextMacro.scala:69)
+    at io.getquill.context.ContextMacro$class.runExpr(ContextMacro.scala:51)
+    at io.getquill.context.ContextMacro$class.run(ContextMacro.scala:21)
+    at io.getquill.context.sql.SqlContextMacro.run(SqlContextMacro.scala:13)
+    mysqlDatabase run topProductNames4
+
+   */
+
+  // ===========================================================================================================================================================
+  // QUERY-2
+  // ===========================================================================================================================================================
+  val topProductNames2 = quote {
+    Categories
+      .join(Products).on(_.id == _.categoryId)
+      .join(Comments).on(_._2.id == _.productId)
+      .map { case (((category, product), comment)) =>
+        (product.name, product.rating)
+      }
+      .sortBy(_._2)
+  }
+
+  /* compile macro output:
+  Information:(_, 17) SELECT x4.name, x4.rating FROM category x3 INNER JOIN product x4 ON x3.id = x4.category_id INNER JOIN comment x6 ON x4.id = x6.product_id ORDER BY x4.rating ASC
+  mysqlDatabase run topProductNames2
+   */
+
+  // ===========================================================================================================================================================
+  // RUN
+  // ===========================================================================================================================================================
+  mysqlDatabase run topProductNames2
+
 
 }
